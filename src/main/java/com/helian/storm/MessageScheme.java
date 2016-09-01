92646b71000dd65ab4d8e3155795e1b3b5f1eefb
an <15501233770@163.com>
2016-09-01 17:49:03 +0800

    add jdk version

--- src/main/java/com/helian/storm/MessageScheme.java
+++ src/main/java/com/helian/storm/MessageScheme.java
@@ -12,6 +12,9 @@ import backtype.storm.tuple.Values;
  *
  */
 public class MessageScheme implements Scheme {
+	
+	private static final long serialVersionUID = -7346046274720549570L;
+
 	public List<Object> deserialize(byte[] ser) {
         try {
             String msg = new String(ser, "UTF-8"); 
